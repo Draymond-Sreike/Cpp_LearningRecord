1991a3aa30eabb73ab4adc83d28410e840fa23e3
Liang Shennhen <641831191@qq.com>
2022-12-01 22:04:30 +0800

    1

--- 运算符重载/运算符重载/递增运算符重载.cpp
+++ 运算符重载/运算符重载/递增运算符重载.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 class MyInteger
 {
+	friend ostream& operator<<(ostream& cout, MyInteger &myInt);
+
 public:
 	/// <summary>
 	/// MyInteger类的无参构造：为了确保程序中定义MyInteger类型的变量（对象）且无赋初值的情况下，
@@ -12,6 +14,7 @@ public:
 	/// </summary>
 	MyInteger()	
 	{
+		cout << "无参构造调用" << endl;
 		value = 0;
 	}
 
@@ -24,45 +27,111 @@ public:
 	/// </param>
 	MyInteger(int for_value)	
 	{
+		cout << "有参构造调用" << endl;
+
 		this->value = for_value;
+	}
+	
+	MyInteger operator++()
+	{
+		cout << "++运算符操作函数调用" << endl;
+
+		value++;	// 写成value = value + 1、value += 1、++value都没有问题，保证这行代码执行结束后value增加1即可
+		return *this;
 	}
 
 private:
 	int value;
 };
 
-void func(int)
-{}
-
-void test01()
+ostream& operator<<(ostream &cout, MyInteger &myInt)
 {
-	//func("src");
-	int a = 0;
-	//cout << (a++)++ << endl;
-	cout << ++a << endl;
-	cout << a << endl;
-	
-	int b = 0;
-	cout << b++ << endl;
-	cout << b << endl;
+	cout << "<<运算符操作函数调用" << endl;
+	cout << myInt.value;
+	return cout;
 }
 
+//void fun1(int& a)
+//{
+//	cout << "形参类型为 int& 的函数调用" << endl;
+//}
+//
+//void fun1(int a)
+//{
+//	cout << "形参类型为 int 的函数调用" << endl;
+//}
+//
+//void fun2(MyInteger a) 
+//{
+//	cout << "形参类型为 MyInteger 的函数调用" << endl;
+//}
+//void fun2(MyInteger& a) 
+//{
+//	cout << "形参类型为 MyInteger& 的函数调用" << endl;
+//}
+
+// 测试函数
+void test01()
+{
+	//MyInteger myInt1 = 0;
+	//MyInteger& myInt2 = myInt1;
+	//fun2(myInt1);
+	//fun2(myInt2);
+
+	int a = 0;
+	int& b = a;
+
+	// 验证函数重载
+	//fun1(&a);
+	//fun1((int&)b);
+
+}
+
+// 测试函数
 void test02()
 {
-	MyInteger myInt1 = 0;
+	//MyInteger myInt1 = 0;
+	//cout << myInt1 << endl;
 	//cout << ++myInt1 << endl;	// 1
-	//cout << myInt1 << endl;		// 1
+	////MyInteger &temp = myInt1.operator++();
+	//operator<<(cout, myInt1.operator++()) << endl;	// 1
 
 	MyInteger myInt2 = 0;
 	//cout << myInt2++ << endl;	// 0
 	//cout << myInt2 << endl;		// 1
 }
 
+int fun2(int &fun2_a)
+{
+	//cout << &fun2_a << endl;	
+
+	fun2_a++;
+	return fun2_a;
+}
+
+ostream& fun1(ostream& cout, int &fun1_a)
+{
+	//cout << &fun1_a << endl;
+
+	cout << fun1_a;
+	return cout;
+}
+
+void test03()
+{
+	int a = 0;
+	//fun1(cout, fun2(a)) << endl;
+	int& b = 10;
+	cout << &a << endl;
+	//cout << &b << endl;
+	//int& b = fun2(a);
+}
+
 int main()
 {
 	//test01();
-	test02();
-
+	//test02();
+	test03();
 	system("pause");
 	return 0;
 }
